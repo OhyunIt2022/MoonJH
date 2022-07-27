88170182a030b870d90b9a5d197eea9be95f253b
MoonJH1028 <107673624+MoonJH1028@users.noreply.github.com>
2022-07-27 17:49:34 +0900

    Add files via upload
    메모장 만들기 읽기 기능

---
+++ memo.py
@@ -0,0 +1,22 @@
+from pickletools import optimize
+
+
+print("hello world")
+print("진행할 작업을 선택해주십시오")
+print("1. 메모작성 2. 메모하기")
+
+option = input()
+
+print(option)  
+
+if option == '1':
+    print("아직 구현중")
+elif option =='2':
+    filename = 'memo.txt'
+    f = open(filename)
+    memo = f.read()
+    print(memo)
+    f.close()
+else:
+    print("잘못 입력하였습니다")
+
